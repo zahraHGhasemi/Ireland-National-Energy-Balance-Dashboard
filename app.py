fdbc9147ce3153fab70a38c9cc54f95b9198c593
zahra <zahra.hajighasemi@ucc.ie>
2026-06-04 14:03:39 +0100

    changing the app.py content

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
-
 import dash
 from callback import register_callbacks
 from dash_layout import dash_layout
 from read_data import get_year_min_max, get_df
 
-if __name__ == '__main__':
-    app = dash.Dash(__name__)
-    filename = "National-Energy-Balance.xlsx"
-    df = get_df(filename)
-    min_year, max_year = get_year_min_max(filename)
-    app.layout = dash_layout(min_year, max_year)
-    register_callbacks(app, df)
-    app.run(debug=True)
+app = dash.Dash(__name__)
 
+filename = "National-Energy-Balance.xlsx"
+df = get_df(filename)
+min_year, max_year = get_year_min_max(filename)
+
+app.layout = dash_layout(min_year, max_year)
+register_callbacks(app, df)
+
+server = app.server  # 👈 ADD THIS LINE
+
+if __name__ == '__main__':
+    app.run(debug=True)
